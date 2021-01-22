39be446f8c0c9221320418eaed1cae1dfc17d729
Haosong Li <hdlee9885@hotmail.com>
2021-01-21 23:21:03 -0600

    Cleaned up

---
+++ components/auth/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+function Landing() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Landing
