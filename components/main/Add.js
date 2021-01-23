ad0b974b8a9253ca5f2286d8836a53a63c4adaed
Haosong Li <hdlee9885@hotmail.com>
2021-01-23 17:17:05 -0600

    Added a camera view

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,10 +1,49 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { Text, View, Button } from 'react-native';
+import { Camera } from 'expo-camera';
 
-export default function Add() {
+export default function App() {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
     return (
-        <View>
-            <Text>Add</Text>
+        <View style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
+                <Camera
+                    style={{ flex: 1}} 
+                    type={type} 
+                />
+            </View>
+
+            <Button
+                style={{
+                    flex: 0.1,
+                    alignSelf: 'flex-end',
+                    alignItems: 'center',
+                }}
+                title="Flip Image"
+                onPress={() => {
+                    setType(
+                        type === Camera.Constants.Type.back
+                        ? Camera.Constants.Type.front
+                        : Camera.Constants.Type.back
+                    );
+                }}>
+                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white'}}> Flip </Text>
+            </Button>
         </View>
-    )
+    );
 }
