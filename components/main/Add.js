ff58843b6f5aeb6bba78c0b839bda2824f3d1023
Haosong Li <hdlee9885@hotmail.com>
2021-01-23 17:39:35 -0600

    Added take picture and select pictures

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,17 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, StyleSheet, Image } from 'react-native';
 import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
     const [hasPermission, setHasPermission] = useState(null);
+    const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
     const [type, setType] = useState(Camera.Constants.Type.back);
+    const [camera, setCamera] = useState(null);
+    const [image, setImage] = useState(null);
 
     useEffect(() => {
         (async () => {
             const { status } = await Camera.requestPermissionsAsync();
             setHasPermission(status === 'granted');
+
+            const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            if (galleryStatus.status !== 'granted') {
+                alert('You need to turn on permissions to visit your photos')
+            } else {
+                setHasGalleryPermission(galleryStatus === 'granted')
+            }
         })();
     }, []);
+
+    const takePicture = async () => {
+        if (camera) {
+            const data = await camera.takePictureAsync(null);
+            setImage(data.uri);
+        }
+    }
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
 
     if (hasPermission === null) {
         return <View />;
@@ -19,21 +52,21 @@ export default function App() {
     if (hasPermission === false) {
         return <Text>No access to camera</Text>;
     }
+    if (hasGalleryPermission === null || hasGalleryPermission === false) {
+        return <View />;
+    }
     return (
         <View style={{ flex: 1 }}>
-            <View style={{ flex: 1 }}>
+            <View style={styles.cameraContainer}>
                 <Camera
-                    style={{ flex: 1}} 
+                    ref={ref => setCamera(ref)}
+                    style={styles.fixedRatio} 
                     type={type} 
+                    ratio={'1:1'}
                 />
             </View>
 
             <Button
-                style={{
-                    flex: 0.1,
-                    alignSelf: 'flex-end',
-                    alignItems: 'center',
-                }}
                 title="Flip Image"
                 onPress={() => {
                     setType(
@@ -44,6 +77,32 @@ export default function App() {
                 }}>
                 <Text style={{ fontSize: 18, marginBottom: 10, color: 'white'}}> Flip </Text>
             </Button>
+
+            <Button 
+                title="Take Picture"
+                onPress={() => takePicture()}
+            />
+
+            <Button
+                title="Select Image"
+                onPress={() => pickImage()}
+            />
+
+            {image && <Image 
+                        source={{ uri: image }} 
+                        style={{flex: 1}}
+                    />}
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    cameraContainer: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    fixedRatio: {
+        flex: 1,
+        aspectRatio: 1
+    }
+})
