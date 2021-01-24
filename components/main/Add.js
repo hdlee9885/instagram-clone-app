106ebbeb4aa2f33c15391eaed11b324036d2a92d
Haosong Li <hdlee9885@hotmail.com>
2021-01-23 22:28:22 -0600

    Fixed picture bug

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,97 +1,79 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, Button, StyleSheet, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
 
-export default function App() {
-    const [hasPermission, setHasPermission] = useState(null);
+
+export default function Add({ navigation }) {
     const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
-    const [type, setType] = useState(Camera.Constants.Type.back);
+    const [hasCameraPermission, setHasCameraPermission] = useState(null);
     const [camera, setCamera] = useState(null);
     const [image, setImage] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
 
     useEffect(() => {
         (async () => {
-            const { status } = await Camera.requestPermissionsAsync();
-            setHasPermission(status === 'granted');
+            const cameraStatus = await Camera.requestPermissionsAsync();
+            setHasCameraPermission(cameraStatus.status === 'granted');
 
             const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
-            if (galleryStatus.status !== 'granted') {
-                alert('You need to turn on permissions to visit your photos')
-            } else {
-                setHasGalleryPermission(galleryStatus === 'granted')
-            }
-        })();
+            setHasGalleryPermission(galleryStatus.status === 'granted');
+
+            })();
     }, []);
 
     const takePicture = async () => {
         if (camera) {
-            const data = await camera.takePictureAsync(null);
-            setImage(data.uri);
+        const data = await camera.takePictureAsync(null);
+        setImage(data.uri);
         }
     }
 
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
-          mediaTypes: ImagePicker.MediaTypeOptions.Images,
-          allowsEditing: true,
-          aspect: [4, 3],
-          quality: 1,
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 1,
         });
-
         console.log(result);
 
         if (!result.cancelled) {
-            setImage(result.uri);
+        setImage(result.uri);
         }
     };
 
-    if (hasPermission === null) {
+
+    if (hasCameraPermission === null || hasGalleryPermission === false) {
         return <View />;
     }
-    if (hasPermission === false) {
+    if (hasCameraPermission === false || hasGalleryPermission === false) {
         return <Text>No access to camera</Text>;
-    }
-    if (hasGalleryPermission === null || hasGalleryPermission === false) {
-        return <View />;
     }
     return (
         <View style={{ flex: 1 }}>
-            <View style={styles.cameraContainer}>
-                <Camera
-                    ref={ref => setCamera(ref)}
-                    style={styles.fixedRatio} 
-                    type={type} 
-                    ratio={'1:1'}
-                />
-            </View>
+        <View style={styles.cameraContainer}>
+            <Camera
+            ref={ref => setCamera(ref)}
+            style={styles.fixedRatio}
+            type={type}
+            ratio={'1:1'} />
+        </View>
 
-            <Button
-                title="Flip Image"
-                onPress={() => {
-                    setType(
-                        type === Camera.Constants.Type.back
-                        ? Camera.Constants.Type.front
-                        : Camera.Constants.Type.back
-                    );
-                }}>
-                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white'}}> Flip </Text>
-            </Button>
-
-            <Button 
-                title="Take Picture"
-                onPress={() => takePicture()}
-            />
-
-            <Button
-                title="Select Image"
-                onPress={() => pickImage()}
-            />
-
-            {image && <Image 
-                        source={{ uri: image }} 
-                        style={{flex: 1}}
-                    />}
+        <Button
+            title="Flip Image"
+            onPress={() => {
+            setType(
+                type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+            );
+            }}>
+        </Button>
+        <Button title="Take Picture" onPress={() => takePicture()} />
+        <Button title="Pick Image From Gallery" onPress={() => pickImage()} />
+        {/* <Button title="Save" onPress={() => navigation.navigate('Save', { image })} /> */}
+        {image && <Image source={{ uri: image }} style={{ flex: 1 }} />}
         </View>
     );
 }
@@ -105,4 +87,5 @@ const styles = StyleSheet.create({
         flex: 1,
         aspectRatio: 1
     }
+
 })
