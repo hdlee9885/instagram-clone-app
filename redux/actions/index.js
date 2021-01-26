41a46d6f32c35b3cb00ff1eb94508ab6a986c336
Haosong Li <hdlee9885@hotmail.com>
2021-01-25 22:52:06 -0600

    Added post feed system

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase'
-import { USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE } from '../constants/index'
+import { USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE, USERS_DATA_STATE_CHANGE, USERS_POSTS_STATE_CHANGE } from '../constants/index'
 
 export function fetchUser() {
     return ((dispatch) => {
@@ -52,5 +52,55 @@ export function fetchUserFollowing() {
             });
             dispatch({ type: USER_FOLLOWING_STATE_CHANGE, following: following});
         })
+    })
+}
+
+export function fetchUsersData(uid) {
+    return ((dispatch, getState) => {
+        const found = getState().usersState.users.some(el => el.uid === uid);
+
+        if (!found) {
+            firebase.firestore()
+                    .collection("users")
+                    .doc(uid)
+                    .get()
+                    .then((snapshot) => {
+                        if (snapshot.exists) {
+                            let user = snapshot.data();
+                            user.uid = snapshot.id;
+
+                            dispatch({ type: USERS_DATA_STATE_CHANGE, user });
+                        }
+                        else {
+                            console.log('does not exist')
+                        }
+                    })
+        }
+    })
+}
+
+export function fetchUsersFollowingPosts(uid) {
+    return ((dispatch, getState) => {
+        firebase.firestore()
+            .collection('posts')
+            .doc(uid)
+            .collection('userPosts')
+            .orderBy('creation', 'asc')
+            .get()
+            .then((snapshot) => {
+                const uid = snapshot.query.EP.path.segments[1];
+                const user = getState().usersState.users.find(el => el.uid === uid);
+
+                let posts = snapshot.docs.map(doc => {
+                    const data = doc.data();
+                    const id = doc.id;
+                    return { id, ...data, user };
+                });
+
+                for(let i = 0; i< posts.length; i++){
+                    dispatch(fetchUsersFollowingLikes(uid, posts[i].id))
+                }
+                dispatch({ type: USERS_POSTS_STATE_CHANGE, posts: posts});
+            })
     })
 }
