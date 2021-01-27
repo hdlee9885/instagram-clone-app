8225e3e6bf311baf2a5c20ef93a0faa81d3aef11
Haosong Li <hdlee9885@hotmail.com>
2021-01-26 23:47:39 -0600

    Added feed posts page

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase'
+require('firebase/firestore')
 import { USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE, USERS_DATA_STATE_CHANGE, USERS_POSTS_STATE_CHANGE } from '../constants/index'
 
 export function fetchUser() {
@@ -42,23 +43,25 @@ export function fetchUserPosts() {
 export function fetchUserFollowing() {
     return ((dispatch) => {
         firebase.firestore()
-        .collection('following')
-        .doc(firebase.auth().currentUser.uid)
-        .collection('userFollowing')
-        .onSnapshot((snapshot) => {
-            let following = snapshot.docs.map(doc => {
-                const id = doc.id;
-                return id;
-            });
-            dispatch({ type: USER_FOLLOWING_STATE_CHANGE, following: following});
-        })
+                .collection("following")
+                .doc(firebase.auth().currentUser.uid)
+                .collection("userFollowing")
+                .onSnapshot((snapshot) => {
+                    let following = snapshot.docs.map(doc => {
+                        const id = doc.id;
+                        return id;
+                    })
+                    dispatch({ type: USER_FOLLOWING_STATE_CHANGE, following });
+                    for (let i = 0; i < following.length; i++) {
+                        dispatch(fetchUsersData(following[i], true));
+                    }
+                })
     })
 }
 
-export function fetchUsersData(uid) {
+export function fetchUsersData(uid, getPosts) {
     return ((dispatch, getState) => {
         const found = getState().usersState.users.some(el => el.uid === uid);
-
         if (!found) {
             firebase.firestore()
                     .collection("users")
@@ -75,6 +78,9 @@ export function fetchUsersData(uid) {
                             console.log('does not exist')
                         }
                     })
+                    if(getPosts){
+                        dispatch(fetchUsersFollowingPosts(uid));
+                    }
         }
     })
 }
@@ -100,7 +106,8 @@ export function fetchUsersFollowingPosts(uid) {
                 for(let i = 0; i< posts.length; i++){
                     dispatch(fetchUsersFollowingLikes(uid, posts[i].id))
                 }
-                dispatch({ type: USERS_POSTS_STATE_CHANGE, posts: posts});
+                dispatch({ type: USERS_POSTS_STATE_CHANGE, posts, uid })
+
             })
     })
 }
