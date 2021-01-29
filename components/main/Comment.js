43b6e194f9858d638467ccaf1462125b70c5a9ab
Haosong Li <hdlee9885@hotmail.com>
2021-01-28 22:50:52 -0600

    Added comments to db

--- components/main/Comment.js
+++ components/main/Comment.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { StyleSheet, View, Text, Image, FlatList, Button } from 'react-native'
 import firebase from 'firebase'
+import { TextInput } from 'react-native-paper';
 require('firebase/firestore')
 
 export default function Comment(props) {
@@ -28,7 +29,20 @@ export default function Comment(props) {
                     });
             setPostId(props.route.params.postId);
         }
-    }, [props.route.params.postId])
+    }, [props.route.params.postId]);
+
+    const onCommentSend = () => {
+        firebase.firestore()
+                .collection('posts')
+                .doc(props.route.params.uid)
+                .collection('userPosts')
+                .doc(pprops.route.params.postId)
+                .collection('comments')
+                .add({
+                    creator: firebase.auth().currentUser.uid,
+                    text: content,
+                })
+    }
 
     return (
         <View>
@@ -42,6 +56,17 @@ export default function Comment(props) {
                     </View>
                 )}
             />
+
+            <View>
+                <TextInput 
+                    placeholder='comment'
+                    onChangeText={(text) => setContent(text)}
+                />
+                <Button 
+                    onPress={() => onCommentSend()}
+                    title="Send"
+                />
+            </View>
         </View>
     )
 }
